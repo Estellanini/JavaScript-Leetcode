06a736513970c40d2f1ed0342e14582133113ca6
Estellanini <1357436238@qq.com>
2021-06-08 20:36:09 +0800

    feat: 字符串：判断回文

--- String/ReverseString.js
+++ String/ReverseString.js
@@ -2,8 +2,16 @@
 //输入： "abcd"   返回值："dcba"
 
 function ReverseString( str ) {
-    console.log(str.split("").reverse().join(""));
+    console.log(str.split(""));//(15) ["I", " ", "a", "m", " ", "a", " ", "s", "t", "u", "d", "e", "n", "t", "."]
+    console.log(str.split(" "));//(4) ["I", "am", "a", "student."]
     return str.split("").reverse().join("");
 }
 
-ReverseString('google');
+ReverseString('I am a student.');
+
+
+function Judge( str ) {
+    console.log(str===str.split("").reverse().join(""))
+    return str===str.split("").reverse().join("");
+}
+Judge("abcba");
