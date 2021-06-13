069859d59fe5f4eacb3a1dd768793550e4579cff
Estellanini <1357436238@qq.com>
2021-06-13 21:44:07 +0800

    feat: 创建 String.prototype.padStart() 方法

--- String/bigIntAdd.js
+++ String/bigIntAdd.js
@@ -1,6 +1,25 @@
 //大数加法
 //以字符串的形式读入两个数字，编写一个函数计算它们的和，以字符串形式返回。
 // （字符串长度不大于100000，保证字符串仅由'0'~'9'这10种字符组成）
+
+//如果原生环境不支持该方法，在其他代码之前先运行下面的代码，将创建 String.prototype.padStart() 方法。
+if (!String.prototype.padStart) {
+    String.prototype.padStart = function padStart(targetLength,padString) {
+        targetLength = targetLength>>0; //floor if number or convert non-number to 0;
+        padString = String((typeof padString !== 'undefined' ? padString : ' '));
+        if (this.length > targetLength) {
+            return String(this);
+        }
+        else {
+            targetLength = targetLength-this.length;
+            if (targetLength > padString.length) {
+                padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+            }
+            return padString.slice(0,targetLength) + String(this);
+        }
+    };
+}
+
 function bigintAdd(s,t){
     let maxLength=Math.max(s.length,t.length);
     s=s.padStart(maxLength,0);
