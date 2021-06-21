02839e816a336f91eff3583fafd9efa34b26ba60
Estellanini <1357436238@qq.com>
2021-06-21 15:17:57 +0800

    feat: 反转字符串增加细节问题

--- String/ReverseString.js
+++ String/ReverseString.js
@@ -7,7 +7,7 @@ function ReverseString( str ) {
     return str.split("").reverse().join("");
 }
 
-ReverseString('I am a student.');
+//ReverseString('I am a student.');
 
 
 function Judge( str ) {
@@ -19,11 +19,21 @@ Judge("abcba");
 
 
 //如果在V8引擎下，并且是ACM模式，需要自己增加输入输出
-var line=readline();
+/*var line=readline();
 function ReverseString(str){
     return str.split(" ").reverse().join(" ");
 }
-console.log(ReverseString(line));
+console.log(ReverseString(line));*/
 
 //或者只用一行代码解决此题
-console.log(readline().split(" ").reverse().join(" "));
+/*console.log(readline().split(" ").reverse().join(" "));*/
+
+//去除空格
+//输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+//如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+function ReverseString1(str){
+    console.log(str.trim().split(' ').filter(item => item!==''))
+    //return str.trim().split(' ').filter(item => item!=='').reverse().join(' ');
+
+}
+console.log(ReverseString1("the sky  is blue"));
