75a589d6283d3b36a69402ecb8869511bef3887f
Estellanini <1357436238@qq.com>
2021-06-24 20:00:33 +0800

    feat: 数学：b个a与a个b相加

---
+++ Math/NNAplusB.js
@@ -0,0 +1,15 @@
+let adda="";
+let addb="";
+function NNAplusB( a ,  b ) {
+    // write code here
+    for(let i=0;i<b;i++){
+        adda+=a;
+    }
+    for(let i=0;i<a;i++){
+        addb+=b;
+    }
+    console.log(adda)
+    return parseInt(adda)+parseInt(addb);
+}
+
+console.log(NNAplusB(1,1))
