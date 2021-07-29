ac7ca131dba48e796ffd5f49a6046282549f52ab
Estellanini <1357436238@qq.com>
2021-07-29 22:57:01 +0800

    feat: 数组：合并有序数组的leetcode解法

--- Array/mergeArray.js
+++ Array/mergeArray.js
@@ -1,3 +1,5 @@
+//合并两个有序数组
+
 /*function merge( A, m, B, n ) {
     if (m===0) return B;
     A=A.slice(0,m);
@@ -7,11 +9,23 @@
 }*/
 
 
-function merge( A, m, B, n ) {
+/*function merge( A, m, B, n ) {
     A.splice(m, A.length - m, ...B)
     A.sort((a, b) =>  a - b)
     return;
 }
 //console.log(merge( [1,2,3,0,0,0],3,[2,5,6],3))//[1, 2, 2, 3, 5, 6]
-console.log(merge([],0,[1,2,3],3))
+console.log(merge([],0,[1,2,3],3))*/
 
+function merge(nums1, m, nums2, n) {
+    return [...nums1.slice(0,m),...nums2].sort((a,b)=>a-b);
+}
+
+console.log(merge([1,2,3,0,0,0],3,[2,5,6],3))//[ 1, 2, 2, 3, 5, 6 ]
+//leetcode解法
+var merge = function(nums1, m, nums2, n) {
+    nums1.splice(m,n);
+    //nums1.push.apply(nums1,nums2);
+    nums1.push(...nums2);
+    nums1.sort((a,b)=>a-b)
+};
